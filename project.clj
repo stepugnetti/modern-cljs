71c2970647e4f4a1e3696b8f6a000078933bbd68
stefano.pugnetti <stefano.pugnetti@sinapsi.com>
2014-11-14 16:01:08 +0100

    Fix exclusions in new libs

--- project.clj
+++ project.clj
@@ -22,11 +22,12 @@
                  [hiccups "0.3.0"]
                  [domina "1.0.3-SNAPSHOT"]
                  [org.clojars.magomimmo/shoreleave-remote "0.3.1-SNAPSHOT"
-                  :exclude [org.clojars.magomimmo/shoreleave-browser]]
+                  :exclusions [org.clojars.magomimmo/shoreleave-browser]]
                  [org.clojars.magomimmo/shoreleave-remote-ring "0.3.1-SNAPSHOT"
-                  :exclude [org.clojars.magomimmo/shoreleave-browser]]
+                  :exclusions [org.clojars.magomimmo/shoreleave-browser]]
                  [org.clojars.magomimmo/shoreleave-browser "0.3.2-SNAPSHOT"]
-                 [org.clojars.stepugnetti/valip "0.4.0-SNAPSHOT"]]
+                 [org.clojars.stepugnetti/valip "0.4.0-SNAPSHOT"
+                  :exclusions [com.keminglabs/cljx]]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-ring "0.8.8"]
