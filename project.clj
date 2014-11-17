a4e12432e5b5aa12ce9c2e3153cb849bb6bb0577
stefano.pugnetti <stefano.pugnetti@sinapsi.com>
2014-11-17 18:09:46 +0100

    Changed cljx target for src/cljx

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :min-lein-version "2.1.2"
 
   ;; clojure source code path
-  :source-paths ["src/clj" "src/cljs" "src/brepl"]
+  :source-paths ["src/clj" "src/cljs" "src/brepl" "target/src/clj" "target/src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
@@ -35,15 +35,14 @@
             [com.keminglabs/cljx "0.4.0"]]
 
   :cljx {:builds [{:source-paths ["src/cljx"]
-                   :output-path "src/clj"
+                   :output-path "target/src/clj"
                    :rules :clj}
                   {:source-paths ["src/cljx"]
-                   :output-path "src/cljs"
+                   :output-path "target/src/cljs"
                    :rules :cljs}]}
 
   ;; enable cljsbuild tasks support
-  :hooks [leiningen.cljsbuild
-          cljx.hooks]
+  :hooks [leiningen.cljsbuild]
 
   ;; ring tasks configuration
   :ring {:handler modern-cljs.core/app}
@@ -54,7 +53,7 @@
   :cljsbuild {:builds
               [{;; build id
                 :id "dev"
-                :source-paths ["src/brepl" "src/cljs"]
+                :source-paths ["target/src/cljs" "src/brepl" "src/cljs"]
 
                 ;; Google Closure Compiler options
                 :compiler {;; the name of emitted JS script file
@@ -66,7 +65,7 @@
                            :pretty-print true}}
                {;; build id
                 :id "pre-prod"
-                :source-paths ["src/brepl" "src/cljs"]
+                :source-paths ["target/src/cljs" "src/brepl" "src/cljs"]
 
                 :compiler {;; different JS output name
                            :output-to "resources/public/js/modern_pre.js"
@@ -78,7 +77,7 @@
                            :pretty-print false}}
                {;; build id
                 :id "prod"
-                :source-paths ["src/cljs"]
+                :source-paths ["target/src/cljs" "src/cljs"]
                 :compiler {;; different JS output name
                            :output-to "resources/public/js/modern.js"
 
