dc3652dbb5798d8329ef6bab5ba59fe9257d44dd
stefano.pugnetti <stefano.pugnetti@sinapsi.com>
2014-11-17 18:09:46 +0100

    Changed cljx target for src/cljx
    Conflicts:
    
    	.gitignore
    	project.clj

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :min-lein-version "2.1.2"
 
   ;; clojure source code path
-  :source-paths ["src/clj" "src/cljs" "src/brepl"]
+  :source-paths ["src/clj" "src/cljs" "src/brepl" "target/src/clj" "target/src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
@@ -34,10 +34,10 @@
             [com.keminglabs/cljx "0.4.0"]]
 
   :cljx {:builds [{:source-paths ["src/cljx"]
-                   :output-path "src/clj"
+                   :output-path "target/src/clj"
                    :rules :clj}
                   {:source-paths ["src/cljx"]
-                   :output-path "src/cljs"
+                   :output-path "target/src/cljs"
                    :rules :cljs}]}
 
   ;; enable cljsbuild tasks support
@@ -50,7 +50,7 @@
   :cljsbuild {:builds
               [{;; build id
                 :id "dev"
-                :source-paths ["src/brepl" "src/cljs"]
+                :source-paths ["target/src/cljs" "src/brepl" "src/cljs"]
 
                 ;; Google Closure Compiler options
                 :compiler {;; the name of emitted JS script file
@@ -62,7 +62,7 @@
                            :pretty-print true}}
                {;; build id
                 :id "pre-prod"
-                :source-paths ["src/brepl" "src/cljs"]
+                :source-paths ["target/src/cljs" "src/brepl" "src/cljs"]
 
                 :compiler {;; different JS output name
                            :output-to "resources/public/js/modern_pre.js"
@@ -74,7 +74,7 @@
                            :pretty-print false}}
                {;; build id
                 :id "prod"
-                :source-paths ["src/cljs"]
+                :source-paths ["target/src/cljs" "src/cljs"]
                 :compiler {;; different JS output name
                            :output-to "resources/public/js/modern.js"
 
